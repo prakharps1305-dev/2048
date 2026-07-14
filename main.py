100f735843246660a395d2917c35ef5063542fe7
Prakhar <prakhar.ps.1305@gmail.com>
2026-07-15 05:01:33 +0530

    spawn, board moves, game over

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 board =[[2,0,0,0],
         [2,0,0,0],
         [4,0,0,0],
@@ -25,13 +26,38 @@ def slide_right(row):
 def transpose(board):
     return [[board[r][c] for r in range(4)] for c in range(4)]
 
-def slide_up(board):
+def move_up(board):
     t = transpose(board)
     t = [slide_left(row) for row in t]     # slide each row left
     return transpose(t)
 
 
-def slide_up(board):
+def move_down(board):
     t = transpose(board)
     t = [slide_right(row) for row in t]     # slide each row left
     return transpose(t)
+
+def spawn(board):
+    empties = [(r, c) for r in range(4) for c in range(4) if board[r][c] == 0]
+    if not empties:
+        return
+    r, c = random.choice(empties)
+    value = 2 if random.random() < 0.9 else 4
+    board[r][c] = value
+
+b = [[0]*4 for _ in range(4)]
+spawn(b)
+spawn(b)
+render(b)
+
+def move_left(board):
+    return [slide_left(row) for row in board]
+
+def move_right(board):
+    return [slide_right(row) for row in board]
+
+=def is_game_over(board):
+    if move_up(board) == board and move_down(board) == board \
+       and move_left(board) == board and move_right(board) == board:
+        return True
+    return False
